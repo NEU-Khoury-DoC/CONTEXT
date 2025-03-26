fdeb67a33af8dbab833ff5cedd913d28cbc59fe5
Mark Fontenot <m.fontenot@northeastern.edu>
2025-03-26 14:28:48 -0400

    Updates to Terraform config.

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "ecs_services" {
     from_port       = 3306
     to_port         = 3306
     protocol        = "tcp"
-    security_groups = [aws_security_group.ecs_services.id]
+    self            = true
   }
 
   # Allow all outbound
@@ -48,26 +48,6 @@ resource "aws_ecs_cluster" "main" {
   name = "monorepo-app-cluster"
 }
 
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_attach" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
 
 resource "aws_ecs_task_definition" "app_task" {
   family                   = "monorepo-app"
@@ -75,7 +55,7 @@ resource "aws_ecs_task_definition" "app_task" {
   network_mode             = "awsvpc"
   cpu                      = "512"
   memory                   = "1024"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = "arn:aws:iam::637423203881:role/ecsTaskExecutionRole"
 
   container_definitions = jsonencode([
     {
@@ -107,6 +87,7 @@ resource "aws_ecs_task_definition" "app_task" {
         { name = "DB_PORT", value = "3306" },
         { name = "DB_USER", value = "root" },
         { name = "DB_PASSWORD", value = "Th1sIs4terr1bleP4ssw0rd!" },
+        { name = "MYSQL_ROOT_PASSWORD", value = "Th1sIs4terr1bleP4ssw0rd!"},
         { name = "DB_NAME", value = "context" }
       ]
     }
